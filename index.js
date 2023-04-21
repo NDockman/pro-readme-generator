86c7045b69ce6117772f6cbfafdf6ea8407cc93d
DockDock <ndockman@comcast.net>
2023-04-20 21:01:28 -0500

    Added questions to index.js.

---
+++ index.js
@@ -0,0 +1,48 @@
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        title: "What is the title of the project? "
+    },
+    {
+        description: "Please enter a description of the project: "
+    },
+    {
+        tableOfContents: "What would you like in the Table of Contents? "
+    },
+    {
+        installation: "Please explain the installation process: "
+    },
+    {
+        usage: "What is the use of your application? "
+    },
+    {
+        license: "What license would you like your application to have? "
+    },
+    {
+        contribution: "How can the user contribute to the repo? "
+    },
+    {
+        test: "What command should be entered to run tests? "
+    },
+    {
+        userQuestions: "What should the user do if they have questions? "
+    }
+];
+
+var questionsIndex = 0;
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    
+}
+
+// Function call to initialize app
+init();
