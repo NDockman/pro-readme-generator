f279c9bf2a0787adf764dec5083159aa43895208
DockDock <ndockman@comcast.net>
2023-04-23 15:58:50 -0500

    Made several changes to the javascript files.

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 
+
+
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//import generateMarkdown from "./utils/generateMarkdown";
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        title: "What is the title of the project? "
-    },
-    {
-        description: "Please enter a description of the project: "
-    },
-    {
-        tableOfContents: "What would you like in the Table of Contents? "
-    },
-    {
-        installation: "Please explain the installation process: "
-    },
-    {
-        usage: "What is the use of your application? "
-    },
-    {
-        license: "What license would you like your application to have? "
-    },
-    {
-        contribution: "How can the user contribute to the repo? "
-    },
-    {
-        test: "What command should be entered to run tests? "
-    },
-    {
-        userQuestions: "What should the user do if they have questions? "
-    }
-];
+
+inquirer
+    .prompt([
+        {
+            name: "title",
+            message: "What is the title of the project? "
+        },
+        {
+            name: "description",
+            message: "Please enter a description of the project: "
+        },
+        {
+            name: "tableOfContents",
+            message: "What would you like in the Table of Contents? "
+        },
+        {
+            name: "installation",
+            message: "Please explain the installation process: "
+        },
+        {
+            name: "usage",
+            message: "What is the use of your application? "
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please select a license for your application: ",
+            choices: ["MIT", "Apache", "GNU"]
+        },
+        {
+            name: "contribution",
+            message: "How can the user contribute to the repo? "
+        },
+        {
+            name: "test",
+            message: "What command should be entered to run tests? "
+        },
+        {
+            name: "userQuestions",
+            message: "What should the user do if they have questions? "
+        }
+    ])
+    .then(response => {
+        const markdownContent = generateMarkdown(response)
+    });
 
 var questionsIndex = 0;
 
@@ -43,6 +65,8 @@ function writeToFile(fileName, data) {
 function init() {
     
 }
+
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,42 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if( license === "MIT" ){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+
+  if (license === "Apache"){
+    return `[![License: Apache](https://img.shields.io/badge/License-Apache-red.svg)](https://opensource.org/license/apache-2-0/)`
+  }
+
+  if (license === "GNU"){
+    return `[![License: GNU](https://img.shields.io/badge/License-GNU-yellowgreen.svg)](https://opensource.org/license/gpl-3-0/)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT"){
+    return `[MIT License Link](https://opensource.org/licenses/MIT)`
+  }
+  
+  if (license === "Apache"){
+    return `[Apache License Link](https://opensource.org/license/apache-2-0/)`
+  }
+
+  if (license === "GNU"){
+    return `[GNU License Link](https://opensource.org/license/gpl-3-0/)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  renderLicenseBadge();
+  renderLicenseLink();
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
